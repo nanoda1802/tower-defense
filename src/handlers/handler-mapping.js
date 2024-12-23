951729b6d16ba01c50d8050a6028a0f05c8c5af8
EuiJoong <euijki87@gmail.com>
2024-12-23 22:23:20 +0900

    update 서버 기동 스켈레톤 코드 추가

---
+++ src/handlers/handler-mapping.js
@@ -0,0 +1,27 @@
+import { gameStart, gameEnd, gameSave, gameLoad } from "./game-handler.js";
+import { collideHandler } from "./headquater-handler.js";
+import { createMonsterHandler, moveMonsterHandler } from "./monster-handler.js";
+import {
+  getTowerHandler,
+  sellTowerHandler,
+  upgradeTowerHandler,
+  attackHandler,
+} from "./tower-handler.js";
+import { waveChangeHandler } from "./wave-handler.js";
+
+const handlerMappings = {
+  11: gameStart,
+  12: gameEnd,
+  13: gameSave,
+  14: gameLoad,
+  21: collideHandler,
+  31: createMonsterHandler,
+  32: moveMonsterHandler,
+  41: getTowerHandler,
+  42: sellTowerHandler,
+  43: upgradeTowerHandler,
+  44: attackHandler,
+  51: waveChangeHandler,
+};
+
+export default handlerMappings;
