42e13734f972d7ecd611beef7a7c8559be51c87c
EuiJoong <euijki87@gmail.com>
2024-12-23 23:25:14 +0900

    update 삭제 함수 추가

--- src/models/tower-model.js
+++ src/models/tower-model.js
@@ -13,6 +13,13 @@ export const setTower = (userId, towerId, positionX, positionY) => {
   return towers[userId].push({ towerId, positionX, positionY });
 };
 
-export const clearTower = (uuuserIdid) => {
+export const clearTower = (userId) => {
   towers[userId] = [];
+};
+
+export const removeTower = (userId, towerId, positionX, positionY) => {
+  const index = towers[userId].findIndex((tower) => tower.towerId === towerId && tower.positionX === positionX && tower.positionY === positionY);
+  if (index !== -1) {
+    return towers[userId].splice(index, 1)[0];
+  }
 };
