3ca2547282041a619053f150bb1e0cb326103ea5
EuiJoong <euijki87@gmail.com>
2024-12-24 15:22:48 +0900

    update 타워 업그레이드 함수 추가

--- src/models/tower-model.js
+++ src/models/tower-model.js
@@ -1,4 +1,5 @@
-const towers = {};
+// const towers = {};
+const towers = { tester: [{ id: 1001, card: 1, color: 'red', positionX: 0, positionY: 0 }] };
 
 //스테이지 초기화
 export const createTower = (userId) => {
@@ -9,8 +10,8 @@ export const getTower = (userId) => {
   return towers[userId];
 };
 
-export const setTower = (userId, towerId, positionX, positionY) => {
-  return towers[userId].push({ towerId, positionX, positionY });
+export const setTower = (userId, towerId, card, color, positionX, positionY) => {
+  return towers[userId].push({ towerId, card, color, positionX, positionY });
 };
 
 export const clearTower = (userId) => {
@@ -18,8 +19,15 @@ export const clearTower = (userId) => {
 };
 
 export const removeTower = (userId, towerId, positionX, positionY) => {
-  const index = towers[userId].findIndex((tower) => tower.towerId === towerId && tower.positionX === positionX && tower.positionY === positionY);
+  const index = towers[userId].findIndex((tower) => tower.id === towerId && tower.positionX === positionX && tower.positionY === positionY);
   if (index !== -1) {
     return towers[userId].splice(index, 1)[0];
+  }
+};
+
+export const upgradeTower = (userId, towerId, positionX, positionY) => {
+  const index = towers[userId].findIndex((tower) => tower.id === towerId && tower.positionX === positionX && tower.positionY === positionY);
+  if (index !== -1) {
+    return (towers[userId][index].card = towers[userId][index].card + 1);
   }
 };
