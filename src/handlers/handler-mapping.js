8d4cc5e97afb216751fee7037a7760842456b704
songwongbin <bb201802@naver.com>
2024-12-26 20:00:19 +0900

    [update] 서버 클라 임시 연결

--- src/handlers/handler-mapping.js
+++ src/handlers/handler-mapping.js
@@ -1,23 +1,33 @@
 import { gameStart, gameEnd, gameSave, gameLoad } from "./game-handler.js";
 import { collideHandler } from "./headquater-handler.js";
-import { createBossHandler, createMonsterHandler, moveMonsterHandler, moveBossHandler } from "./monster-handler.js";
-import { getTowerHandler, sellTowerHandler, upgradeTowerHandler, attackHandler } from "./tower-handler.js";
+import {
+  createBossHandler,
+  createMonsterHandler,
+  moveMonsterHandler,
+  moveBossHandler,
+} from "./monster-handler.js";
+import {
+  getTowerHandler,
+  sellTowerHandler,
+  upgradeTowerHandler,
+  attackHandler,
+} from "./tower-handler.js";
 import { waveChangeHandler } from "./wave-handler.js";
 
 const handlerMappings = {
-  11: gameStart,
-  12: gameEnd,
+  11: gameStart, // 해써
+  12: gameEnd, // 위치만 잡아 놓음
   13: gameSave,
   14: gameLoad,
   21: collideHandler,
-  31: createMonsterHandler,
+  31: createMonsterHandler, // 해써
   32: createBossHandler,
-  33: moveMonsterHandler,
+  33: moveMonsterHandler, // 해써
   34: moveBossHandler,
-  41: getTowerHandler,
-  42: sellTowerHandler,
-  43: upgradeTowerHandler,
-  44: attackHandler,
+  41: getTowerHandler, // 해써
+  42: sellTowerHandler, // 해써
+  43: upgradeTowerHandler, // 해써
+  44: attackHandler, // 위치만 잡아 놓음
   51: waveChangeHandler,
 };
 
--- src/models/monster-model.js
+++ src/models/monster-model.js
@@ -1,92 +1,108 @@
-
 // 몬스터의 현재 위치정보, 체력, 아이디, 생성시간, 스테이지아이디, 보유 골드, (여기서 인덱스 만들기)
 
 // 보스의 현재 위치정보, 체력, 아이디, 생성시간, 스테이지 아이디, 보유 골드, (인덱스 필요없음)
 
-const aliveMonsters = []
+const aliveMonsters = {};
 
-const deathMonsters = []
+const deathMonsters = [];
 
+const aliveBosses = [];
 
-const aliveBosses = []
+const deathBosses = [];
 
-const deathBosses = []
-
-
+export const createAliveMonsters = (userId) => {
+  aliveMonsters[userId] = [];
+};
 
 //살아있는 몬스터 데이터 가져오기
 export const getAliveMonsters = (userId) => {
-    return aliveMonsters[userId];
-}
+  return aliveMonsters[userId];
+};
 
 //살아있는 몬스터 데이터 저장하기
-export const setAliveMonsters = (userId, timestamp, monsterId, monsterIndex, monsterHealth, monsterAttack, monsterSpeed, monsterGold, monsterScore) => {
-    return aliveMonsters[userId].push({
-        timestamp,
-        monsterId,
-        monsterIndex,
-        monsterHealth,
-        monsterAttack,
-        monsterSpeed,
-        monsterGold,
-        monsterScore
-    });
-}
+export const setAliveMonsters = (
+  userId,
+  timestamp,
+  monsterId,
+  monsterIndex,
+  monsterHealth,
+  monsterAttack,
+  monsterSpeed,
+  monsterGold,
+  monsterScore,
+) => {
+  return aliveMonsters[userId].push({
+    timestamp,
+    monsterId,
+    monsterIndex,
+    monsterHealth,
+    monsterAttack,
+    monsterSpeed,
+    monsterGold,
+    monsterScore,
+  });
+};
 
 //죽은 몬스터 데이터 가져오기
 export const getDeathMonsters = (userId) => {
-    return deathMonsters[userId];
-}
+  return deathMonsters[userId];
+};
 
 //죽은 몬스터 데이터 저장하기
 export const setDeathMonsters = (userId) => {
-    return deathMonsters[userId].push({
-        timestamp,
-        monsterId,
-        monsterIndex,
-        monsterHealth,
-        monsterAttack,
-        monsterSpeed,
-        monsterGold,
-        monsterScore
-    });
-}
-
-
-
+  return deathMonsters[userId].push({
+    timestamp,
+    monsterId,
+    monsterIndex,
+    monsterHealth,
+    monsterAttack,
+    monsterSpeed,
+    monsterGold,
+    monsterScore,
+  });
+};
 
 //살아있는 보스 데이터 가져오기
 export const getAliveBosses = (userId) => {
-    return aliveBosses[userId];
-}
+  return aliveBosses[userId];
+};
 
 //살아있는 보스 데이터 저장하기
-export const setAliveBosses = (userId, timestamp, bossId, bossHealth, bossAttack, bossSpeed, bossGold, bossScore) => {
-    return aliveBosses[userId].push({
-        timestamp,
-        bossId,
-        bossHealth,
-        bossAttack,
-        bossSpeed,
-        bossGold,
-        bossScore
-    });
-}
+export const setAliveBosses = (
+  userId,
+  timestamp,
+  bossId,
+  bossHealth,
+  bossAttack,
+  bossSpeed,
+  bossGold,
+  bossScore,
+) => {
+  return aliveBosses[userId].push({
+    timestamp,
+    bossId,
+    bossHealth,
+    bossAttack,
+    bossSpeed,
+    bossGold,
+    bossScore,
+  });
+};
 
 //죽은 보스 데이터 가져오기
 export const getDeathBosses = (userId) => {
-    return deathBosses[userId];
-}
+  return deathBosses[userId];
+};
 
 //죽은 보스 데이터 저장하기
 export const setDeathBosses = (userId) => {
-    return deathBosses[userId].push({
-        timestamp,
-        bossId,
-        bossHealth,
-        bossAttack,
-        bossSpeed,
-        bossGold,
-        bossScore
-    });
-}
+  return deathBosses[userId].push({
+    timestamp,
+    bossId,
+    bossHealth,
+    bossAttack,
+    bossSpeed,
+    bossGold,
+    bossScore,
+  });
+};
