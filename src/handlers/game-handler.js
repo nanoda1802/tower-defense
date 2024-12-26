1f6f8becd5a7710399d7cb1c6990761101e3cb32
황종하 <whdgk3615@naver.com>
2024-12-27 08:54:15 +0900

    [add] HQ handler

--- src/handlers/game-handler.js
+++ src/handlers/game-handler.js
@@ -15,6 +15,7 @@ export const gameStart = (userId, payload) => {
   clearWave(userId);
   // 첫 번째 스테이지(id: 1)로 설정하고 시작 시간 기록
   // setWave(userId, wave.data[0].id, payload.timestamp);
+  
   // 초기 골드 설정 지금은 편의상 100으로 설정
   console.log("웨 않됨???????", userId);
   //타워 초기화
@@ -40,11 +41,7 @@ export const gameEnd = (userId, payload) => {
   const { timestamp: gameEndTime, score, leftGold } = payload;
 
   // payload 타입 검사
-  if (
-    typeof payload.timestamp !== "number" ||
-    typeof payload.score !== "number" ||
-    typeof payload.leftGold !== "number"
-  ) {
+  if (typeof payload.timestamp !== "number" || typeof payload.score !== "number" || typeof payload.leftGold !== "number") {
     throw new Error("잘못된 payload 형식");
   }
 
@@ -101,14 +98,13 @@ export const gameEnd = (userId, payload) => {
   console.log(bossKillScore);
   // 남은 골드 계산
 
-  // 클라이언트에서 가져온 골드와 서버에서 계산된 골드 비교
-  const clientGold = getGold(userId); // 클라이언트에서 가져온 골드
-  if (clientGold !== leftGoldScore) {
-    console.error(
-      `클라이언트 골드 ${clientGold}와 서버 골드 ${leftGoldScore}가 다릅니다. ${clientGold - leftGoldScore} 차이가 납니다.`,
-    );
-    return;
-  }
+  /* 클라이언트에서 가져온 골드와 서버에서 계산된 골드 비교
+   const clientGold = getGold(userId); // 클라이언트에서 가져온 골드
+   if (clientGold !== leftGoldScore) {
+     console.error(`클라이언트 골드 ${clientGold}와 서버 골드 ${leftGoldScore}가 다릅니다. ${clientGold - leftGoldScore} 차이가 납니다.`);
+     return;
+   }
+  */
 
   leftGoldScore = leftGold;
 
@@ -118,9 +114,7 @@ export const gameEnd = (userId, payload) => {
 
   //여기서 나온 스코어가 위의 받아온 클라이언트 스코어가 같은지 확인
   if (serverScore !== score) {
-    console.error(
-      `클라이언트 점수 ${score}와 서버 점수 ${serverScore}가 다릅니다. ${score - serverScore} 차이가 납니다.`,
-    );
+    console.error(`클라이언트 점수 ${score}와 서버 점수 ${serverScore}가 다릅니다. ${score - serverScore} 차이가 납니다.`);
     return;
   }
 
--- src/handlers/headquater-handler.js
+++ src/handlers/headquater-handler.js
@@ -1,3 +1,12 @@
-/* CollideHandler 21 */
-export const collideHandler = () => {
+import { getGameAssets } from "../inits/assets.js";
+
+// x1000 y300 베이스 좌표
+//1000을 넘으면 총돌
+// 헤드퀸터 충돌 처리
+export const collideHandler = (userid, payload) => {
+  const { monsters } = getGameAssets();
+  //지금 몬스터 마다 공격력을 가지고 있다.충돌했을떄 hp 감소
+
+  //근데 일단 몬스터 충돌 처리 해야함
+  //본부 좌표는 고정되어 있지만 몬스터 좌표는 계속 변경되어 있음
 };
