59ac191ec1f0df21874b2617958a0d3f8009a84b
songwongbin <bb201802@naver.com>
2024-12-26 15:55:22 +0900

    [add] 이미지 파일 추가

--- public/src/tower.js
+++ public/src/tower.js
@@ -1,10 +1,11 @@
 export class Tower {
-  constructor(x, y, cost) {
+  constructor(x, y, cost, towerImage) {
     // 생성자 안에서 타워들의 속성을 정의한다고 생각하시면 됩니다!
     this.x = x; // 타워 이미지 x 좌표
     this.y = y; // 타워 이미지 y 좌표
-    this.width = 78; // 타워 이미지 가로 길이 (이미지 파일 길이에 따라 변경 필요하며 세로 길이와 비율을 맞춰주셔야 합니다!)
-    this.height = 150; // 타워 이미지 세로 길이
+    this.image = towerImage;
+    this.width = 100; // 타워 이미지 가로 길이 (이미지 파일 길이에 따라 변경 필요하며 세로 길이와 비율을 맞춰주셔야 합니다!)
+    this.height = 100; // 타워 이미지 세로 길이
     this.attackPower = 40; // 타워 공격력
     this.range = 300; // 타워 사거리
     this.cost = cost; // 타워 구입 비용
@@ -13,14 +14,14 @@ export class Tower {
     this.target = null; // 타워 광선의 목표
   }
 
-  draw(ctx, towerImage) {
-    ctx.drawImage(towerImage, this.x, this.y, this.width, this.height);
+  draw(ctx) {
+    ctx.drawImage(this.image, this.x, this.y, this.width, this.height);
     if (this.beamDuration > 0 && this.target) {
       ctx.beginPath();
       ctx.moveTo(this.x + this.width / 2, this.y + this.height / 2);
       ctx.lineTo(
         this.target.x + this.target.width / 2,
-        this.target.y + this.target.height / 2
+        this.target.y + this.target.height / 2,
       );
       ctx.strokeStyle = "skyblue";
       ctx.lineWidth = 10;
