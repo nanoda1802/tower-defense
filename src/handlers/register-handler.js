dffad817fd1a20a916fdf2bf0691eaaac57d6290
EuiJoong <euijki87@gmail.com>
2024-12-27 12:31:47 +0900

    fix Disconnection userUUID 매개변수 값 제거

--- src/handlers/register-handler.js
+++ src/handlers/register-handler.js
@@ -1,21 +1,21 @@
-import { addUser } from "../models/user-model.js";
-import { handleDisconnect, handlerEvent, handleConnection } from "./helper.js";
+import { addUser } from '../models/user-model.js';
+import { handleDisconnect, handlerEvent, handleConnection } from './helper.js';
 // import { waveChangeHandler } from "../handlers/wave-handler.js";
 
 const registerHandler = (io) => {
-  io.on("connection", async (socket) => {
+  io.on('connection', async (socket) => {
     let userId = socket.handshake.query.userId;
 
     addUser({ userId, socketId: socket.id });
-    console.log("!!!register!!!", userId);
+    console.log('!!!register!!!', userId);
     handleConnection(socket, userId);
 
     //Event
-    socket.on("event", (data) => handlerEvent(io, socket, data));
+    socket.on('event', (data) => handlerEvent(io, socket, data));
 
     //Disconnect
-    socket.on("disconnect", () => {
-      handleDisconnect(socket, userUUID);
+    socket.on('disconnect', () => {
+      handleDisconnect(socket);
     });
   });
 };
