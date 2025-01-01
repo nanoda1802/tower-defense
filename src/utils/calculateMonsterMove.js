2b4d6e55a5043c57c866506d52f5949176b617b8
kimjehoon <kms5064@naver.com>
2025-01-01 22:54:44 +0900

    [fix] 주석정리

--- src/utils/calculateMonsterMove.js
+++ src/utils/calculateMonsterMove.js
@@ -6,8 +6,7 @@ export const calculateMonsterMove = (monsterId, monsterIndex, timestamp) => {
   const speed = monsters.data.find((monster) => monster.id === monsterId).speed; // assets파일에서 speed 가져오기
   console.log("서버시간 : 클라시간", Date.now(), timestamp);
   console.log("시간차 ", Date.now() - timestamp);
-  x = speed * (Date.now() - timestamp); //현재시간 - 소환시간(ms) * 0.0625(프레임단위로 변환)
-  //speed가 2일때, 프레임당 x값 0.125씩 증가
+  x = speed * (Date.now() - timestamp); //현재시간 - 소환시간(ms) * 스피드(게임에셋에서 가져오기)
   if (x >= 1200) {
     x = 1200;
   }
